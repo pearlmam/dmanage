4f88cac6cbbb4a2da6b10918b33046a07bdf4fc6
Marcus Pearlman <marcuspearlman@boisestate.edu>
2026-09-02 19:45:19 -0600

    add comment to config about options

--- dmanage/config.py
+++ dmanage/config.py
@@ -1,3 +1,10 @@
-# -*- coding: utf-8 -*-
+from typing import Literal
 
-PARALLEL_BACKEND = "multiprocessing"
+PARALLEL_BACKEND: Literal["multiprocessing", "multiprocess", "pickle", "dill"] = "multiprocessing"
+"""The execution backend for parallel operations.
+
+Supported options:
+
+* ``'multiprocessing'`` or ``'pickle'``: Standard library multiprocessing.
+* ``'multiprocess'`` or ``'dill'``: Extended serialization using ``dill``.
+"""

0774a3a81b4e08d9d09e48076b4bbc24f552b23d
Marcus Pearlman <marcuspearlman@boisestate.edu>
2026-09-18 13:07:59 -0600

    move PanelServer and helpers to _base.py
    This separates the viz flavor from the server classes.

--- dmanage/viz/__init__.py
+++ dmanage/viz/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from dmanage.viz._hvplot2 import *
+from dmanage.viz._base import *
 
 from dmanage.viz._hvplot2 import __all__ as _hvplot_all
-
-__all__ = _hvplot_all
+from dmanage.viz._base import __all__ as _base_all
+__all__ = _hvplot_all + _base_all
